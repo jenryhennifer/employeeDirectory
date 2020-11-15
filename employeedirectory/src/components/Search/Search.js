7ee14cb3d2e5d20878de2f8e433367de605757ee
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-14 19:35:34 -0800

    sort by name, species, position, and status working

--- employeedirectory/src/components/Search/Search.js
+++ employeedirectory/src/components/Search/Search.js
@@ -3,14 +3,14 @@ import './style.css';
 
 function Search() {
   return (
-    <div class="md-form mt-0">
+    <div className="md-form mt-0">
       <input
-        class="form-control"
+        className="form-control"
         type="text"
         placeholder="Search"
         aria-label="Search"
       />
-      <button type="submit" class="btn btn-primary mb-2">
+      <button type="submit" className="btn btn-primary mb-2">
         Search
       </button>
     </div>
--- employeedirectory/src/components/Table/Table.js
+++ employeedirectory/src/components/Table/Table.js
@@ -2,20 +2,37 @@ import React from 'react';
 import './style.css';
 
 function Table({ employees }) {
-  
+  const [sorted, setSorted] = React.useState(null);
+  console.log(sorted)
+
+  const sortedEmployees = sorted ? employees.concat().sort((a, b) => {
+    const aval = a[sorted];
+    const bval = b[sorted];
+    if (aval < bval) {
+      return -1;
+    } else if (bval < aval) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }) : employees;
+  console.log(sortedEmployees)
+
   return (
     <table>
       <tr>
-        <th>Image</th>
-        <th>Name</th>
-        <th>Position</th>
-        <th>Species</th>
-        <th>Status</th>
+        <th>
+          <div>Image</div>
+        </th>
+        <th><div onClick={()=> setSorted('name')}>Name</div></th>
+        <th><div onClick={()=> setSorted('position')}>Position</div></th>
+        <th><div onClick={() => setSorted('species')}>Species</div></th>
+        <th><div onClick={() => setSorted('status')}>Status</div></th>
       </tr>
-      {employees.map((employee) => (
-        <tr>
+      {sortedEmployees.map((employee) => (
+        <tr key={employee.id}>
           <td>
-            <img class="employeeID" src={employee.image}></img>
+            <img className="employeeID" src={employee.image}></img>
           </td>
           <td>{employee.name}</td>
           <td>{employee.position}</td>
